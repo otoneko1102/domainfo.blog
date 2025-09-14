8cade1e4e9009de78e82b24ef2e5ed9e8b4fe0db
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-09-14 19:04:01 +0200

    ビルド方法を変更

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import { createHtmlPlugin } from "vite-plugin-html";
+import { createMultiHtmlPlugin } from "vite-plugin-multi-html"
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
@@ -10,20 +11,31 @@ export default defineConfig(({ mode }) => {
       port: Number(env.PORT) || 5173,
     },
     plugins: [
-      createHtmlPlugin({
-        minify: true,
-        inject: {
-          data: {
-            serviceName: env?.VITE_SERVICE_NAME,
-            serviceDescription: env?.VITE_SERVICE_DESCRIPTION,
-            serviceHeaderImage: env?.VITE_SERVICE_HEADER_IMAGE,
-            serviceIconImage: env?.VITE_SERVICE_ICON_IMAGE,
-            serviceThumbnailImage: env?.VITE_SERVICE_THUMBNAIL_IMAGE,
-            serviceAuthor: env.VITE_SERVICE_AUTHOR,
-            serviceStartYear: env.VITE_SERVICE_START_YEAR,
-          },
-        },
+      createMultiHtmlPlugin({
+        variables: {
+          serviceName: env?.VITE_SERVICE_NAME,
+          serviceDescription: env?.VITE_SERVICE_DESCRIPTION,
+          serviceHeaderImage: env?.VITE_SERVICE_HEADER_IMAGE,
+          serviceIconImage: env?.VITE_SERVICE_ICON_IMAGE,
+          serviceThumbnailImage: env?.VITE_SERVICE_THUMBNAIL_IMAGE,
+          serviceAuthor: env.VITE_SERVICE_AUTHOR,
+          serviceStartYear: env.VITE_SERVICE_START_YEAR,
+        }
       }),
+      // createHtmlPlugin({
+      //   minify: true,
+      //   inject: {
+      //     data: {
+      //       serviceName: env?.VITE_SERVICE_NAME,
+      //       serviceDescription: env?.VITE_SERVICE_DESCRIPTION,
+      //       serviceHeaderImage: env?.VITE_SERVICE_HEADER_IMAGE,
+      //       serviceIconImage: env?.VITE_SERVICE_ICON_IMAGE,
+      //       serviceThumbnailImage: env?.VITE_SERVICE_THUMBNAIL_IMAGE,
+      //       serviceAuthor: env.VITE_SERVICE_AUTHOR,
+      //       serviceStartYear: env.VITE_SERVICE_START_YEAR,
+      //     },
+      //   },
+      // }),
     ],
     build: {
       outDir: "../lib/components",

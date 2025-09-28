a32be38d3fa1bae1b0de53e5fc018129d8a3cf51
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-09-28 16:32:32 +0200

    拡張子を追加・コードフォーマット

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig, loadEnv } from "vite";
-import { createMultiHtmlPlugin } from "vite-plugin-multi-html"
+import { createMultiHtmlPlugin } from "vite-plugin-multi-html";
 import prismjsPlugin from "vite-plugin-prismjs";
 
 export default defineConfig(({ mode }) => {
@@ -20,11 +20,11 @@ export default defineConfig(({ mode }) => {
           serviceThumbnailImage: env?.VITE_SERVICE_THUMBNAIL_IMAGE,
           serviceAuthor: env.VITE_SERVICE_AUTHOR,
           serviceStartYear: env.VITE_SERVICE_START_YEAR,
-        }
+        },
       }),
       prismjsPlugin({
         languages: "all",
-      })
+      }),
     ],
     build: {
       chunkSizeWarningLimit: 1000,
@@ -35,13 +35,17 @@ export default defineConfig(({ mode }) => {
           entryFileNames: "assets/[hash].js",
           chunkFileNames: "assets/[hash].js",
           assetFileNames: (i) => {
-            if (/\.(a?png|jpe?g|gif|webp|mp3|m4a|mp4|mov|webm)$/.test(i.name)) {
+            if (
+              /\.(a?png|jpe?g|gif|webp|tiff?|bmp|heif|heic|pdf|mp4|mov|webm|ogv|mov|qt|avi|flv|mpe?g|mkv|m2ts|wmv|asf|vob|mp3|wav|weba|m4a|oga|ogg|opus|aac|midi?|rmi?|aiff|flac|alac|wma)$/.test(
+                i.name,
+              )
+            ) {
               return "assets/img/[name][extname]";
             }
             if (/\.(ttf|woff2?)$/.test(i.name)) {
-              return "assets/fonts/[name][extname]"
+              return "assets/fonts/[name][extname]";
             }
-            return "assets/[hash][extname]"
+            return "assets/[hash][extname]";
           },
           manualChunks(id) {
             if (id.includes("node_modules/katex")) {
@@ -50,9 +54,9 @@ export default defineConfig(({ mode }) => {
             if (id.includes("node_modules/prismjs")) {
               return "prism";
             }
-          }
-        }
-      }
+          },
+        },
+      },
     },
   };
 });
